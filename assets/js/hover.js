ccad18f2ee10ea09cb0990585de60107d2c7bb0e
BuildTools <sarquentin@gmail.com>
2020-11-19 17:45:15 +0100

    Update

---
+++ assets/js/hover.js
@@ -0,0 +1,45 @@
+
+$( "#expertise-box" )
+.on( "mouseenter", function() {
+  $( "#expertise" ).css('display', 'flex');
+})
+.on( "mouseleave", function() {
+  $( "#expertise" ).css('display', 'none');
+});
+
+$( "#accompagnement-box" )
+.on( "mouseenter", function() {
+  $( "#accompagnement" ).css('display', 'flex');
+})
+.on( "mouseleave", function() {
+  $( "#accompagnement" ).css('display', 'none');
+});
+$( "#installation-box" )
+.on( "mouseenter", function() {
+  $( "#installation" ).css('display', 'flex');
+})
+.on( "mouseleave", function() {
+  $( "#installation" ).css('display', 'none');
+});
+$( "#expertise" )
+  .on( "mouseenter", function() {
+    $( "#expertise" ).css('display', 'flex');
+  })
+  .on( "mouseleave", function() {
+    $( "#expertise" ).css('display', 'none');
+  });
+
+$( "#accompagnement" )
+  .on( "mouseenter", function() {
+    $( "#accompagnement" ).css('display', 'flex');
+  })
+  .on( "mouseleave", function() {
+    $( "#accompagnement" ).css('display', 'none');
+  });
+  $( "#installation" )
+  .on( "mouseenter", function() {
+    $( "#installation" ).css('display', 'flex');
+  })
+  .on( "mouseleave", function() {
+    $( "#installation" ).css('display', 'none');
+  });
