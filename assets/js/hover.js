5d569f9bc464a35ee9f71d17ace39b7f5ffb6d7f
BuildTools <sarquentin@gmail.com>
2021-01-04 14:40:08 +0100

    Update

--- assets/js/hover.js
+++ assets/js/hover.js
@@ -1,53 +1,21 @@
 
 $( "#expertise-box" )
-.on( "mouseenter", function() {
-  if ($(window).width() <= 768) return;
-  $( "#expertise" ).css('display', 'flex');
-})
-.on( "mouseleave", function() {
-  if ($(window).width() <= 768) return;
-  $( "#expertise" ).css('display', 'none');
-});
+.on( "mouseenter", () => {if ($(window).width() > 751) $( ".srv-desc-content #expertise" ).css('display', 'flex');})
+.on( "mouseleave", () => {if ($(window).width() > 751) $( ".srv-desc-content #expertise" ).css('display', 'none');});
 
 $( "#accompagnement-box" )
-.on( "mouseenter", function() {
-  if ($(window).width() <= 768) return;
-  $( "#accompagnement" ).css('display', 'flex');
-})
-.on( "mouseleave", function() {
-  if ($(window).width() <= 768) return;
-  $( "#accompagnement" ).css('display', 'none');
-});
-$( "#installation-box" )
-.on( "mouseenter", function() {
-  if ($(window).width() <= 768) return;
-  $( "#installation" ).css('display', 'flex');
-})
-.on( "mouseleave", function() {
-  if ($(window).width() <= 768) return;
-  $( "#installation" ).css('display', 'none');
-});
-$( "#expertise" )
-  .on( "mouseenter", function() {
-  if ($(window).width() <= 768) return;
-  $( "#expertise" ).css('display', 'flex');
-  })
-  .on( "mouseleave", function() {
-  if ($(window).width() <= 768) return;
-  $( "#expertise" ).css('display', 'none');
-  });
+.on( "mouseenter", () => {if ($(window).width() > 751) $( ".srv-desc-content #accompagnement" ).css('display', 'flex');})
+.on( "mouseleave", () => {if ($(window).width() > 751) $( ".srv-desc-content #accompagnement" ).css('display', 'none');});
 
-$( "#accompagnement" )
-  .on( "mouseenter", function() {
-    $( "#accompagnement" ).css('display', 'flex');
-  })
-  .on( "mouseleave", function() {
-    $( "#accompagnement" ).css('display', 'none');
-  });
-  $( "#installation" )
-  .on( "mouseenter", function() {
-    $( "#installation" ).css('display', 'flex');
-  })
-  .on( "mouseleave", function() {
-    $( "#installation" ).css('display', 'none');
-  });
+$( "#installation-box" )
+.on( "mouseenter", () => { if ($(window).width() > 751) $( ".srv-desc-content #installation" ).css('display', 'flex');})
+.on( "mouseleave", () => {if ($(window).width() > 751) $( ".srv-desc-content #installation" ).css('display', 'none');});
+
+$("#expertise-box").on( "click", () => {if ($(window).width() < 751) $( "#m-expertise" ).css('display', 'flex');})
+$("#m-expertise").on("click", () => { if ($(window).width() < 751) $( "#m-expertise" ).css('display', 'none') })
+
+$("#accompagnement-box").on( "click", () => {if ($(window).width() < 751) $( "#m-accompagnement" ).css('display', 'flex');})
+$("#m-accompagnement").on("click", () => { if ($(window).width() < 751) $( "#m-accompagnement" ).css('display', 'none') })
+
+$("#installation-box").on( "click", () => {if ($(window).width() < 751) $( "#m-installation" ).css('display', 'flex');})
+$("#m-installation").on("click", () => { if ($(window).width() < 751) $( "#m-installation" ).css('display', 'none') })
